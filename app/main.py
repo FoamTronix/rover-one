a90942358ba6ea60642f357cba04867225b0fcfa
weshays <weshays@gbdev.com>
2016-08-20 18:56:04 -0700

    Updated files for testing

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return 'Hello, World!'
+@app.route('/<msg>')
+def index(msg=None):
+    return render_template('index.html', msg = msg)
 
 if __name__ == "__main__":
     app.run()
