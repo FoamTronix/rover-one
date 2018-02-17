80421f8260a8a949a153ca7b0d23efeb58b827e3
weshays <weshays@gbdev.com>
2018-02-16 18:49:26 -0800

    Set TTY port

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ import serial
 import time
 import RPi.GPIO as GPIO
 
-ser = serial.Serial('/dev/ttyUSB0', 9600)
+ser = serial.Serial('/dev/ttyACM0', 9600)
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(pin, GPIO.OUT)
