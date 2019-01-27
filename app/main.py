bfa3f60527601c0f60d6d81ff2abe41233dd17c4
Wes Hays <weshays@gmail.com>
2019-01-27 10:36:19 -0800

    Removed video for now to see if the errors stop happening

--- app/main.py
+++ app/main.py
@@ -81,18 +81,18 @@ def sensors():
 
 # Camera
 # -------------------------------------------------
-@app.route('/video_feed')
-def video_feed():
-  return Response(gen(VideoCamera()),
-                  mimetype='multipart/x-mixed-replace; boundary=frame')
-
-def gen(camera):
-  while True:
-    frame = camera.get_frame()
-    yield (b'--frame\r\n'
-           b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+# @app.route('/video_feed')
+# def video_feed():
+#   return Response(gen(VideoCamera()),
+#                   mimetype='multipart/x-mixed-replace; boundary=frame')
+#
+# def gen(camera):
+#   while True:
+#     frame = camera.get_frame()
+#     yield (b'--frame\r\n'
+#            b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 # -------------------------------------------------
 
 if __name__ == "__main__":
-  app.run(host='192.168.1.79', threaded=True) # debug=True)
+  app.run(host='192.168.1.79', threaded=True, debug=True)
   # app.run(host='0.0.0.0', threaded=True)
