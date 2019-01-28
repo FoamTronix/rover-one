1250b33dff7a6da56fd07e03c68347c72ad7d010
weshays <weshays@gbdev.com>
2019-01-27 22:11:56 -0800

    Added back camera

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template, redirect, jsonify, Response
-#from camera import VideoCamera
+from camera import VideoCamera
 
 import json
 import serial
@@ -81,16 +81,16 @@ def sensors():
 
 # Camera
 # -------------------------------------------------
-# @app.route('/video_feed')
-# def video_feed():
-#   return Response(gen(VideoCamera()),
-#                   mimetype='multipart/x-mixed-replace; boundary=frame')
-#
-# def gen(camera):
-#   while True:
-#     frame = camera.get_frame()
-#     yield (b'--frame\r\n'
-#            b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+@app.route('/video_feed')
+def video_feed():
+  return Response(gen(VideoCamera()),
+                  mimetype='multipart/x-mixed-replace; boundary=frame')
+
+def gen(camera):
+  while True:
+    frame = camera.get_frame()
+    yield (b'--frame\r\n'
+           b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 # -------------------------------------------------
 
 if __name__ == "__main__":
